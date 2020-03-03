c3e67d511edca18293562f5f41f9f2f06ab96b47
Vincent Arlou <arlou@protonmail.com>
2020-03-02 22:46:41 -0500

    true to false

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router} from 'react-router-dom';
 import { useRoutes } from './routes';
 
 function App() {
-  const routes = useRoutes(true);
+  const routes = useRoutes(false);
 
   return(
     <Router>
